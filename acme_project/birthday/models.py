e4811736ce48d56fe3df2b69938be4aa7398e317
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-07-30 18:19:31 +0500

    added bootstrap5 changed base, birthday, index html created Birthday model

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,12 @@
+"""Birthday model."""
 from django.db import models
+
+
+class Birthday(models.Model):
+    """Birthday model."""
+
+    first_name: models.CharField = models.CharField('Имя', max_length=20)
+    last_name: models.CharField = models.CharField(
+        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+    )
+    birthday: models.DateField = models.DateField('Дата рождения')
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,9 +1,17 @@
+"""Birthday views."""
 from django.shortcuts import render
 
 from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
-    form = BirthdayForm()
+    """Birthday view."""
+    form = BirthdayForm(request.GET or None)
     context = {'form': form}
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
