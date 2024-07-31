86fe311b03777d5d30a23fb97b02b13ff9a6b4e7
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-07-31 19:11:17 +0500

    use_l1on false, added urls: edit and delete added condition in birthday() for edit created delete_birthday() changed birthday_list and birthday html

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,14 +1,18 @@
 """Birthday views."""
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
-from .utils import calculate_birthday_countdown
 from .models import Birthday
+from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
+def birthday(request, pk=None):
     """Birthday view."""
-    form = BirthdayForm(request.POST or None)
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {'form': form}
     if form.is_valid():
         form.save()
@@ -24,3 +28,14 @@ def birthday_list(request):
     birthdays = Birthday.objects.all()
     context = {'birthdays': birthdays}
     return render(request, 'birthday/birthday_list.html', context)
+
+
+def delete_birthday(request, pk):
+    """Delete birthday view."""
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
+    return render(request, 'birthday/birthday.html', context)
