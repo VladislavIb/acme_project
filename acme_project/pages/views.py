716be5517c256f63dd2a1f0ec3aea6856bbc7142
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-08-06 16:18:22 +0500

    add CBV to pages

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+"""Pages views."""
+from django.views.generic import TemplateView
+
+from birthday.models import Birthday
 
 
-def homepage(request):
-    return render(request, 'pages/index.html')
+class HomePage(TemplateView):
+    """Home page view."""
+
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        """Get context data."""
+        context = super().get_context_data(**kwargs)
+        context['total_count'] = Birthday.objects.count()
+        return context
