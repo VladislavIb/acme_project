b02026fa608a6188e3b1c01279f0dc6c40dd974f
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-08-08 20:08:14 +0500

    add new path for auth/registration change header with registration button add registration_form

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -2,12 +2,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 from django.contrib.auth.views import (
     LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView,
     PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView,
     PasswordResetCompleteView
 )
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -17,53 +20,62 @@ urlpatterns = [
     path(
         'login/',
         LoginView.as_view(template_name='registration/login.html'),
-        name='login'
+        name='login',
     ),
     path(
         'logout/',
         LogoutView.as_view(template_name='registration/logged_out.html'),
-        name='logout'
+        name='logout',
     ),
     path(
         'password_change/',
         PasswordChangeView.as_view(
             template_name='registration/password_change_form.html'
         ),
-        name='password_change'
+        name='password_change',
     ),
     path(
         'password_change/done/',
         PasswordChangeDoneView.as_view(
             template_name='registration/password_change_done.html'
         ),
-        name='password_change_done'
+        name='password_change_done',
     ),
     path(
         'password_reset/',
         PasswordResetView.as_view(
             template_name='registration/password_reset_form.html'
         ),
-        name='password_reset'
+        name='password_reset',
     ),
     path(
         'password_reset/done/',
         PasswordResetDoneView.as_view(
             template_name='registration/password_reset_done.html'
         ),
-        name='password_reset_done'
+        name='password_reset_done',
     ),
     path(
         'reset/<uidb64>/<token>/',
         PasswordResetConfirmView.as_view(
             template_name='registration/password_reset_confirm.html'
         ),
-        name='password_reset_confirm'
+        name='password_reset_confirm',
     ),
     path(
         'reset/done/',
         PasswordResetCompleteView.as_view(
             template_name='registration/password_reset_complete.html'
         ),
-        name='password_reset_complete'
+        name='password_reset_complete',
+    ),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
